1efaf8f9ca9b8db425f5ac3a9f5c154a41e3fc3b
Kevin Forbes <forbesk@email.arizona.edu>
2013-02-17 08:38:43 -0700

    Bugfix on Arduino super params
    Now connects as expected and is writing data to Arduino.

--- CATfish/src/org/auvua/catfish/Arduino.java
+++ CATfish/src/org/auvua/catfish/Arduino.java
@@ -6,6 +6,6 @@ public class Arduino extends SerialHardware {
 
 	public Arduino(String port_name, int timeout, int baud_rate) {
 		super(port_name, timeout, baud_rate, SerialPort.DATABITS_8,
-				SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+				SerialPort.PARITY_NONE, SerialPort.STOPBITS_1);
 	}
 }
