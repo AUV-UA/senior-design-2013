dbf43ccb2abc85193c5fef712cac8953a4f589cb
Kevin Forbes <forbesk@email.arizona.edu>
2013-02-19 13:18:20 -0700

    Implemented custom serialEvent

--- CATfish/src/org/auvua/catfish/Arduino.java
+++ CATfish/src/org/auvua/catfish/Arduino.java
@@ -1,5 +1,7 @@
 package org.auvua.catfish;
 
+import java.util.logging.Level;
+
 import gnu.io.*;
 
 public class Arduino extends SerialHardware {
@@ -7,5 +9,48 @@ public class Arduino extends SerialHardware {
 	public Arduino(String port_name, int timeout, int baud_rate) {
 		super(port_name, timeout, baud_rate, SerialPort.DATABITS_8,
 				SerialPort.PARITY_NONE, SerialPort.STOPBITS_1);
+	}
+	
+	@Override
+	public synchronized void serialEvent(SerialPortEvent event) {
+		switch (event.getEventType()) {
+		case SerialPortEvent.BI:
+		case SerialPortEvent.OE:
+		case SerialPortEvent.FE:
+		case SerialPortEvent.PE:
+		case SerialPortEvent.CD:
+		case SerialPortEvent.CTS:
+		case SerialPortEvent.DSR:
+		case SerialPortEvent.RI:
+		case SerialPortEvent.OUTPUT_BUFFER_EMPTY:
+			break;
+		case SerialPortEvent.DATA_AVAILABLE:
+			try {
+				byte[] data = new byte[18];
+				int c;
+				int bytes_read = 0;
+				
+				//find beginning of message
+				while((char)input.read() != '*');
+				data[0] = '*';
+				
+				//read all bytes
+				while(bytes_read < 17 && (c = input.read()) >= 0) {
+					bytes_read++;
+					data[bytes_read] = (byte)(c & 0xff);
+				}
+				
+				//clear the input buffer in case any stray messages are left
+				//while(input.ready())
+				//	input.skip(1);
+
+				if((char)data[17] == '-') sendHardwareEvent(data);
+			} catch (Exception e) {
+				String msg = String.format(
+						"Unable to read from input stream.\n %s", e.toString());
+				LOGGER.log(Level.SEVERE, msg);
+			}
+			break;
+		}
 	}
 }
