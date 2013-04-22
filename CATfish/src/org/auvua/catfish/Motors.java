de073892686c8800b7a3e9e2bdd224f63dcef96b
Kevin Forbes <forbesk@email.arizona.edu>
2013-04-22 14:19:22 -0700

    Motors extends Arduino.

--- CATfish/src/org/auvua/catfish/Motors.java
+++ CATfish/src/org/auvua/catfish/Motors.java
@@ -2,5 +2,10 @@ package org.auvua.catfish;
 
 import gnu.io.*;
 
-public class Motors {	
+public class Motors extends Arduino {
+
+	public Motors(String port_name, int timeout, int baud_rate) {
+		super(port_name, timeout, baud_rate);
+		// TODO Auto-generated constructor stub
+	}	
 }
