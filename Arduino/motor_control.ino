f3dacba1836012a3c7abf327233ad1313774f155
Evan Briones <erbriones@gmail.com>
2013-04-23 01:48:33 -0700

    Added support for soft limiting.

--- Arduino/motor_control.ino
+++ Arduino/motor_control.ino
@@ -2,7 +2,7 @@
  * motor_control - Bridge between motor control and
  * desired motor signals.
  *
- * Author(s): Evan Briones
+ * Author(s): Evan Briones and Kevin Forbes
  *
  * Copyright (C) 2013 AUVUA Club
  *
@@ -15,14 +15,23 @@
 #define RXPIN 3
 
 #define BAUD_RATE 19200
-#define PACKET_LENGTH 20
 #define TIMEOUT_THRESHOLD 1000
 
-// Motor control
+#define EMPTY_BYTE 0x00
+#define PACKET_LENGTH 20
 #define STOP_SPEED 0
+#define FORWARD_ACCELERATION_LIMIT 5
+#define REVERSE_ACCELERATION_LIMIT 9
+#define DECELERATION_LIMIT 2
+#define LIMIT_VALUE 2
+
+// Flags
+#define READY_FLAG 0x7F
+#define SAFE_START_FLAG 0x83
 #define CONTROL_FLAG 0xAA
-#define MOTOR_FWD 0x05
-#define MOTOR_REV 0x06
+#define LIMIT_FLAG 0x22
+#define FORWARD_FLAG 0x05
+#define REVERSE_FLAG 0x06
 
 long current_time;
 long previous_time;
@@ -30,22 +39,35 @@ SoftwareSerial serial_controller = SoftwareSerial(RXPIN, TXPIN);
 
 /*
  * Set the speed between 0 and 200 which is
- * percentage reverse or forward (-100 and 100) 
- * centered around 100 (0 percent). 
+ * percentage reverse or forward (-100 and 100)
+ * centered around 100 (0 percent).
  */
-void setMotorSpeed(byte dir, byte speed, byte device_id) {
+void setMotorSpeed(uint8_t dir, uint8_t speed, uint8_t device_id) {
     byte packet[5] = {0};
-    
+
     speed = speed < 0 ? 0 : (speed > 100 ? 100 : speed);
     speed = speed / 2;
-    
+
     packet[0] = CONTROL_FLAG;
     packet[1] = device_id;
-    packet[2] = (dir == 1) ? MOTOR_REV : MOTOR_FWD;
-    packet[3] = 0x00;
+    packet[2] = (dir == 1) ? REVERSE_FLAG : FORWARD_FLAG;
+    packet[3] = EMPTY_BYTE;
     packet[4] = speed;
-    
+
     serial_controller.write(packet, 5);
+}
+
+void setMotorLimit(uint8_t device_id, uint8_t limit_type, int limit_value) {
+    byte packet[6] = {0};
+
+    packet[0] = CONTROL_FLAG;
+    packet[1] = device_id;
+    packet[2] = LIMIT_FLAG;
+    packet[3] = limit_type;
+    packet[4] = limit_value % 128;
+    packet[5] = limit_value >> 7;
+
+    serial_controller.write(packet, 6);
 }
 
 void setup() {
@@ -58,39 +80,41 @@ void setup() {
 
     // Setup timer
     current_time = previous_time = millis();
-    
+
     // Start motor controllers
-    serial_controller.write(0x83);
-    delay(1);
+    serial_controller.write(SAFE_START_FLAG);
+    delay(5);
+
+    for (uint8_t i = 0; i < 6; i++) {
+        setMotorLimit(i, FORWARD_ACCELERATION_LIMIT, LIMIT_VALUE);
+        setMotorLimit(i, REVERSE_ACCELERATION_LIMIT, LIMIT_VALUE);
+    }
 }
 
 void loop() {
-    
+
     // Controller doesn't send back info yet.
     if (serial_controller.available()) {
     }
-    
-    if(Serial.peek() != 127) {
+
+    if(Serial.peek() != READY_FLAG) {
       Serial.read();
     } else if (Serial.available() > PACKET_LENGTH) {
         Serial.read();
-        
         byte packet[PACKET_LENGTH] = {0};
+
         for (int i = 0; i < PACKET_LENGTH; i++) {
           packet[i] = Serial.read();
         }
-        
-        //while(Serial.available())
-        //  Serial.read();
-        
+
         //if (get_checksum(packet) > 0)
         //    return;
+
         for (int i = 0; i < PACKET_LENGTH - 2; i += 3) {
             setMotorSpeed(packet[i+1], packet[i+2], packet[i]);
         }
 
-        serial_controller.write(0x83);
-
+        serial_controller.write(SAFE_START_FLAG);
         previous_time = millis();
     }
 
@@ -100,8 +124,6 @@ void loop() {
     }
 
     current_time = millis();
-    
-    //serial_controller.write(0x83);
 }
 
 int get_checksum(byte packet[]) {
@@ -112,5 +134,5 @@ int get_checksum(byte packet[]) {
     }
 
     checksum -= word(packet[PACKET_LENGTH-2], packet[PACKET_LENGTH-1]);
-    return int(checksum);
+    return checksum;
 }
