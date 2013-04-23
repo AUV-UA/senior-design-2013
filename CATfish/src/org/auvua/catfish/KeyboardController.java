bc9680e9d8e14a14139c6fa752f2aafdce4820d0
Kevin Forbes <forbesk@email.arizona.edu>
2013-04-22 17:44:48 -0700

    Added motionmap. Added key scanning.

--- CATfish/src/org/auvua/catfish/KeyboardController.java
+++ CATfish/src/org/auvua/catfish/KeyboardController.java
@@ -3,12 +3,23 @@ package org.auvua.catfish;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 public class KeyboardController extends Controller implements KeyListener {
 	HashMap<Integer, Boolean> keymap;
+	final HashMap<Integer, MotionVector> motionmap;
 
 	public KeyboardController() {
 		keymap = new HashMap<Integer, Boolean>();
+		motionmap = new HashMap<Integer, MotionVector>();
+		motionmap.put(KeyEvent.VK_W, new MotionVector(0f, 1f, 0f, 0f, 1f));
+		motionmap.put(KeyEvent.VK_A, new MotionVector(-1f, 0f, 0f, 0f, 1f));
+		motionmap.put(KeyEvent.VK_S, new MotionVector(0f, -1f, 0f, 0f, 1f));
+		motionmap.put(KeyEvent.VK_D, new MotionVector(1f, 0f, 0f, 0f, 1f));
+		motionmap.put(KeyEvent.VK_UP, new MotionVector(0f, 0f, 1f, 0f, 1f));
+		motionmap.put(KeyEvent.VK_LEFT, new MotionVector(0f, 0f, 0f, -1f, 1f));
+		motionmap.put(KeyEvent.VK_DOWN, new MotionVector(0f, 0f, -1f, 0f, 1f));
+		motionmap.put(KeyEvent.VK_RIGHT, new MotionVector(0f, 0f, 0f, 1f, 1f));
 	}
 
 	@Override
@@ -26,8 +37,13 @@ public class KeyboardController extends Controller implements KeyListener {
 			case KeyEvent.VK_DOWN:
 			case KeyEvent.VK_RIGHT:
 			case KeyEvent.VK_LEFT:
-				m = new MotionVector(0f, 0f, 0f, 0f);
+				m = new MotionVector(0f, 0f, 0f, 0f, 0.5f);
 				
+				for(Entry<Integer, Boolean> entry : keymap.entrySet()) {
+					if(motionmap.containsKey(entry.getKey()) && entry.getValue()) {
+						m.addVector(motionmap.get(entry.getKey()));
+					}
+				}
 				
 				break;
 		}
