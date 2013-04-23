713fd99b31ebc3144ab5ee8d77d96f32380b4217
Kevin Forbes <forbesk@email.arizona.edu>
2013-04-23 11:49:40 -0700

    Added motor re-mapping and comments.

--- CATfish/src/org/auvua/catfish/Motors.java
+++ CATfish/src/org/auvua/catfish/Motors.java
@@ -1,9 +1,13 @@
 package org.auvua.catfish;
 
-import java.util.Arrays;
-
-
 public class Motors extends Arduino {
+	
+	private final int MOTOR_ID_FL = 2;
+	private final int MOTOR_ID_FR = 4;
+	private final int MOTOR_ID_RL = 1;
+	private final int MOTOR_ID_RR = 3;
+	private final int MOTOR_ID_VL = 0;
+	private final int MOTOR_ID_VR = 5;
 
 	public Motors(String port_name, int timeout, int baud_rate, CATFishModel model) {
 		super(port_name, timeout, baud_rate, model);
@@ -12,15 +16,20 @@ public class Motors extends Arduino {
 	@Override
 	public void send(char[] data) {
 		byte[] msg = new byte[21];
-		msg[0] = 127;
+
+		msg[0] = 127;		//Ready byte of 127, for indexing.
+		
+		//Bytes sent per motor are 3: first is motor id, second is direction bit (1 = backwards),
+		//   and third is speed between 0 and 100 percent.
 		for(byte i = 0; i < data.length; i++) {
 			msg[3*i+1] = i;
 			msg[3*i+2] = (byte)(data[i] > 127 ? 1 : 0);
 			msg[3*i+3] = (byte)(data[i] > 127 ? -data[i] : data[i]);
 		}
-		msg[19] = msg[20] = 0x00;
-		System.out.println(Arrays.toString(msg));
-		write(msg);
+	
+		msg[19] = msg[20] = 0x00;	//checksum bytes (not implemented yet)
+
+		write(msg);					//write the packet
 	}
 
 	public void update(MotionVector v) {
@@ -42,11 +51,12 @@ public class Motors extends Arduino {
 		float max_motor_speed = Math.max(Math.abs(fl), Math.max(Math.abs(fr), Math.max(Math.abs(rl), Math.abs(rr))));
 		float motor_scaling = speed * (max_motor_speed == 0 ? 0 : 1.0f/max_motor_speed);
 		
-		speeds[0] = (char)(fl * motor_scaling);
-		speeds[1] = (char)(fr * motor_scaling);
-		speeds[2] = (char)(rl * motor_scaling);
-		speeds[3] = (char)(rr * motor_scaling);
-		speeds[4] = speeds[5] = (char)(vert * speed);
+		speeds[MOTOR_ID_FL] = (char)(fl * motor_scaling);
+		speeds[MOTOR_ID_FR] = (char)(fr * motor_scaling);
+		speeds[MOTOR_ID_RL] = (char)(rl * motor_scaling);
+		speeds[MOTOR_ID_RR] = (char)(rr * motor_scaling);
+		speeds[MOTOR_ID_VL] = (char)(vert * speed);
+		speeds[MOTOR_ID_VR] = (char)(vert * speed);
 		
 		send(speeds);
 	}
