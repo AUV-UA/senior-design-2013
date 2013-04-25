cad24e3d595060d25e7466f7286c49de83148b59
Evan Briones <erbriones@gmail.com>
2013-04-25 13:43:41 -0700

    Added mplabx project for the pinger localization board.

---
+++ Embedded/PingerLocalization/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PingerLocalization.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PingerLocalization.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=pingerlocalization.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/pingerlocalization.tar
