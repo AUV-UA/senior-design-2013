4d41e1a1e93e38a55cd37a507fa8cfcef07ccedb
Kevin Forbes <forbesk@email.arizona.edu>
2013-04-29 11:59:21 -0700

    Added AUVState and AUVCommand interfaces.

--- Agent/src/org/auvua/agent/Task.java
+++ Agent/src/org/auvua/agent/Task.java
@@ -3,6 +3,8 @@ package org.auvua.agent;
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.logging.Logger;
+
+import org.auvua.utils.protobuffer.AUVprotocol.AUVCommand;
 import org.auvua.utils.protobuffer.AUVprotocol.AUVState;
 
 public abstract class Task implements Runnable {
@@ -11,7 +13,7 @@ public abstract class Task implements Runnable {
 			.getLogger(Logger.GLOBAL_LOGGER_NAME);
 	protected ArrayList<Parameter> params;
 	protected ConcurrentLinkedQueue<AUVState> current_state;
-	protected AUVState output;
+	protected AUVCommand output;
 	protected int num_params;
 	protected String name;
 	protected Location start;
@@ -21,13 +23,14 @@ public abstract class Task implements Runnable {
 		params = new ArrayList<Parameter>();
 		current_state = new ConcurrentLinkedQueue<AUVState>();
 		output = null;
+		finished = false;
 	}
 
 	public final void queueState(AUVState state) {
 		current_state.add(state);
 	}
 	
-	public final AUVState getOutput() {
+	public final AUVCommand getOutput() {
 		return output;
 	}
 
@@ -41,5 +44,17 @@ public abstract class Task implements Runnable {
 
 	public void setParameters(ArrayList<Parameter> params) {
 		this.params = params;
+	}
+	
+	public AUVCommand buildAUVCommand(float x, float y, float z, float rot, float speed) {
+		AUVCommand.Builder command = AUVCommand.newBuilder();
+		command.setSpeed(AUVCommand.Movement.newBuilder()
+				.setX(x)
+				.setY(y)
+				.setZ(z)
+				.setYaw(rot)
+				.setPower(speed));
+		
+		return command.build();
 	}
 }
