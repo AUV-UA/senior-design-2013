9a785657a512030044ea0444bacabf1b2be1845d
Kevin Forbes <forbesk@email.arizona.edu>
2013-04-29 12:22:14 -0700

    Finalized driving straight task.

--- Agent/src/org/auvua/agent/DriveStraight.java
+++ Agent/src/org/auvua/agent/DriveStraight.java
@@ -7,31 +7,40 @@ import org.auvua.utils.protobuffer.AUVprotocol.AUVState;
 public class DriveStraight extends Task {
 
 	public DriveStraight() {
-		finished = false;
+		super();
 		name = "DriveStraight";
 		num_params = 2;
-		current_state = new ConcurrentLinkedQueue<AUVState>();
 	}
 
 	@Override
 	public void run() {
+		float distance = (Float) params.get(0).getData();
+		float speed = (Float) params.get(1).getData();
+		
+		long timeout = System.currentTimeMillis() + (long)(1000.0f*distance + 5000.0f/speed);
+		
+		LOGGER.info("Starting task: " + name);
+		
 		while (!isFinished()) {
 			if (current_state.isEmpty())
 				continue;
 			
 			AUVState state = current_state.remove();
 			
-			LOGGER.info("Starting task: " + name);
+			output = buildAUVCommand(0.0f, 1.0f, 0.0f, 0.0f, speed);
+
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(50);
 			} catch (InterruptedException e) {
 				LOGGER.warning("Task forced to close. Stopping the robot...");
 				return;
 			}
 			
-			LOGGER.info("Task '" + name + "' complete");
-			finished = true;
+			finished = (System.currentTimeMillis() > timeout);
 		}
+		output = buildAUVCommand(0.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+		
+		LOGGER.info("Task '" + name + "' complete");
 	}
 
 }
