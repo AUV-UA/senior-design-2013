954d8bc4eef9b10e7948671f8820f440f768f58c
Evan Briones <erbriones@gmail.com>
2013-04-29 11:22:54 -0700

    Task now can send and recieve an AUVState.

--- Agent/src/org/auvua/agent/DriveStraight.java
+++ Agent/src/org/auvua/agent/DriveStraight.java
@@ -1,24 +1,37 @@
 package org.auvua.agent;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import org.auvua.utils.protobuffer.AUVprotocol.AUVState;
+
 public class DriveStraight extends Task {
 
 	public DriveStraight() {
 		finished = false;
 		name = "DriveStraight";
 		num_params = 2;
+		current_state = new ConcurrentLinkedQueue<AUVState>();
 	}
-	
+
 	@Override
 	public void run() {
-		LOGGER.info("Starting task: " + name);
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			LOGGER.warning("Task forced to close. Stopping the robot...");
-			return;
+		while (!isFinished()) {
+			if (current_state.isEmpty())
+				continue;
+			
+			AUVState state = current_state.remove();
+			
+			LOGGER.info("Starting task: " + name);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				LOGGER.warning("Task forced to close. Stopping the robot...");
+				return;
+			}
+			
+			LOGGER.info("Task '" + name + "' complete");
+			finished = true;
 		}
-		LOGGER.info("Task '" + name + "' complete");
-		finished = true;
 	}
-	
+
 }
--- Agent/src/org/auvua/agent/Task.java
+++ Agent/src/org/auvua/agent/Task.java
@@ -1,28 +1,45 @@
 package org.auvua.agent;
 
 import java.util.ArrayList;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.logging.Logger;
+import org.auvua.utils.protobuffer.AUVprotocol.AUVState;
 
 public abstract class Task implements Runnable {
 
 	public static final Logger LOGGER = Logger
 			.getLogger(Logger.GLOBAL_LOGGER_NAME);
 	protected ArrayList<Parameter> params;
+	protected ConcurrentLinkedQueue<AUVState> current_state;
+	protected AUVState output;
 	protected int num_params;
 	protected String name;
 	protected Location start;
 	protected boolean finished;
+
+	protected Task() {
+		params = new ArrayList<Parameter>();
+		current_state = new ConcurrentLinkedQueue<AUVState>();
+		output = null;
+	}
+
+	public final void queueState(AUVState state) {
+		current_state.add(state);
+	}
 	
+	public final AUVState getOutput() {
+		return output;
+	}
+
 	public boolean isFinished() {
 		return finished;
 	}
-	
+
 	public void setStartLocation(Location start) {
 		this.start = start;
 	}
-	
+
 	public void setParameters(ArrayList<Parameter> params) {
 		this.params = params;
 	}
-
 }
